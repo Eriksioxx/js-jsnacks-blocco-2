9d31bf54fb967235721e8e5357462ce0ec48c256
erik schievenin <erik.schievenin@gmail.com>
2022-03-11 21:17:55 +0100

    Full exercises complete

--- jsnack3/script.js
+++ jsnack3/script.js
@@ -5,3 +5,16 @@ console.log('JS OK!');
 // fino a quando ne avrà tanti quanti l’altro.
 
 
+// Crea due array che hanno un numero di elementi diversi.
+let numberGroup1 = [1, 2, 3];
+
+let numberGroup2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// Aggiungi elementi casuali all’array che ha meno elementi
+
+for (let i = 0; numberGroup1.length < numberGroup2.length; i++) {
+    numberGroup1.push(parseInt(Math.random() * 10) + 1);
+}
+
+console.log('Numeri generati: ' + numberGroup1);
+console.log('Numeri generati: ' + numberGroup2);
