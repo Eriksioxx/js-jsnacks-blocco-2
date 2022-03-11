8e3bd0f89e380ea7ef0efb50a78087b9787482a5
erik schievenin <erik.schievenin@gmail.com>
2022-03-11 18:11:06 +0100

    Exercise 1 start

---
+++ jsnack1/script.js
@@ -0,0 +1,12 @@
+console.log('JS OK!');
+
+// Crea un array di numeri interi e fai la somma
+// di tutti gli elementi che sono in posizione dispari.
+
+let numeri = [];
+
+for (let i = 0; i < 10; i++) {
+    numeri.push(parseInt(Math.random() * 100) + 1);
+}
+console.log(numeri);
+
