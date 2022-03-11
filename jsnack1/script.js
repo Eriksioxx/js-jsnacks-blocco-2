83d56ecc1f29304d77b4e203827f7a62eb222acf
erik schievenin <erik.schievenin@gmail.com>
2022-03-11 18:32:17 +0100

    Exercise 1 complete

--- jsnack1/script.js
+++ jsnack1/script.js
@@ -4,9 +4,24 @@ console.log('JS OK!');
 // di tutti gli elementi che sono in posizione dispari.
 
 let numeri = [];
-
+// genero numeri casuali da 0 a 99
 for (let i = 0; i < 10; i++) {
     numeri.push(parseInt(Math.random() * 100) + 1);
 }
-console.log(numeri);
 
+console.log('Numeri generati: ' + numeri);
+
+
+
+
+// faccio la somma di tutti gli elementi che sono in posizione dispari
+let sommaDispari = 0;
+
+for (let i = 0; i < numeri.length; i++) {
+
+    if (i % 2 !== 0) {
+        sommaDispari += numeri[i];
+    }
+}
+
+console.log('La somma delle posizioni dispari è : ' + sommaDispari);
