475329bd99b0bb64a6ef9c3b24a50f3041dc627b
erik schievenin <erik.schievenin@gmail.com>
2022-03-11 19:43:45 +0100

    2 exercise complete

--- jsnack2/script.js
+++ jsnack2/script.js
@@ -1,4 +1,46 @@
 console.log('JS OK!');
 
-// Crea un array di numeri interi e fai la somma
-// di tutti gli elementi che sono in posizione dispari.
+// Il software deve chiedere per 5 volte all’utente di inserire un numero.
+// Il programma stampa la somma di tutti i numeri inseriti.
+// Esegui questo programma in due versioni, con il for e con il while.
+
+
+// Il software deve chiedere per 5 volte all’utente di inserire un numero.
+let requestNumber = [];
+for (let i = 0; i < 5; i++) {
+
+    requestNumber.push(parseInt(prompt('Inserisci numero...')));
+}
+console.log('Numeri inseriti sono: ' + requestNumber);
+
+
+// Il programma stampa la somma di tutti i numeri inseriti.
+let somma = 0;
+for (let i = 0; i < requestNumber.length; i++) {
+
+    somma += requestNumber[i];
+}
+
+console.log('La somma di tutti i numeri inseriti è : ' + somma);
+
+// Esegui questo programma con il while.
+
+/*
+let requestNumber = [];
+
+let j = 0;
+while (j < 5) {
+    requestNumber.push(parseInt(prompt('Inserisci numero...')));
+    j++;
+}
+console.log('Numeri inseriti sono: ' + requestNumber);
+
+let somma = 0;
+let i = 0;
+while (i < requestNumber.length) {
+
+    somma += requestNumber[i];
+    i++;
+}
+console.log('La somma di tutti i numeri inseriti è : ' + somma);
+*/
